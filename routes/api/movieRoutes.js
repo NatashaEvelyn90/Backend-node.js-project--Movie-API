a4f5772e4983b72f3652bb62796c5f10ce91b22d
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-10-31 16:33:26 -0500

    pushing code
    able to get the movies pages to load. added some light bootstrap to make them look nice. Tomorrow will have it to only load a certain amount of movies onto the page.
    
    must update personal notes and .txt file

--- routes/api/movieRoutes.js
+++ routes/api/movieRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const axios = require('axios')
 const PORT = process.env.PORT || 4200
 
-//! Animation MOVIES 
+//! ANIMATION MOVIES 
 //? http://localhost:4200/animation
 router.get('/animation', (req, res)=> {
     const url = 'https://api.sampleapis.com/movies/animation'
@@ -14,6 +14,118 @@ router.get('/animation', (req, res)=> {
             res.render('pages/allMovies', {
                 title: 'Animation Movies',
                 name: 'Animation Movies',
+                data: resp.data
+            })
+        })
+})
+
+//! CLASSIC MOVIES 
+//? http://localhost:4200/classic
+router.get('/classic', (req, res) => {
+    const url= 'https://api.sampleapis.com/movies/classic'
+
+    axios.get(url)
+    .then(resp => {
+            res.render('pages/allMovies', {
+                title: 'Classic Movies',
+                name: 'Classic Movies',
+                data: resp.data
+            })
+        })
+})
+
+//! COMEDY MOVIES
+//? http://localhost:4200/comedy 
+
+router.get('/comedy', (req, res) => {
+    const url= 'https://api.sampleapis.com/movies/comedy'
+
+    axios.get(url)
+    .then(resp => {
+            res.render('pages/allMovies', {
+                title: 'Comedy Movies',
+                name: 'Comedy Movies',
+                data: resp.data
+            })
+        })
+})
+
+//! DRAMA MOVIES
+//? http://localhost:4200/drama 
+
+router.get('/drama', (req, res) => {
+    const url= 'https://api.sampleapis.com/movies/drama'
+
+    axios.get(url)
+    .then(resp => {
+            res.render('pages/allMovies', {
+                title: 'Drama Movies',
+                name: 'Drama Movies',
+                data: resp.data
+            })
+        })
+})
+
+//! HORROR MOVIES
+//? http://localhost:4200/horror 
+
+router.get('/horror', (req, res) => {
+    const url= 'https://api.sampleapis.com/movies/horror'
+
+    axios.get(url)
+    .then(resp => {
+            res.render('pages/allMovies', {
+                title: 'Horror Movies',
+                name: 'Horror Movies',
+                data: resp.data
+            })
+        })
+})
+
+//! FAMILY MOVIES 
+//? http://localhost:4200/family 
+
+router.get('/family', (req, res) => {
+    const url= 'https://api.sampleapis.com/movies/family'
+
+    axios.get(url)
+    .then(resp => {
+            res.render('pages/allMovies', {
+                title: 'Family Movies',
+                name: 'Family Movies',
+                data: resp.data
+            })
+        })
+})
+
+//! MYSTERY MOVIES
+//? http://localhost:4200/mystery 
+
+router.get('/mystery', (req, res) => {
+    const url= 'https://api.sampleapis.com/movies/mystery'
+
+    axios.get(url)
+    .then(resp => {
+            res.render('pages/allMovies', {
+                title: 'Mystery Movies',
+                name: 'Mystery Movies',
+                data: resp.data
+            })
+        })
+})
+
+//! WESTERN MOVIES 
+//? http://localhost:4200/western 
+
+router.get('/western', (req, res) => {
+    const url= 'https://api.sampleapis.com/movies/western'
+
+    axios.get(url)
+    .then(resp => {
+            res.render('pages/allMovies', {
+                title: 'Western Movies',
+                name: 'Western Movies',
+                data: resp.data
             })
         })
 })
--- routes/router.js
+++ routes/router.js
@@ -9,11 +9,20 @@ const PORT = process.env.PORT || 4200
 router.get('/', (req, res)=> {
     res.render('pages/home', {
                 //* the title is going to show in your header.ejs + home.ejs.  You are using EJS for this! Name is showing in home.ejs
-                title: "MovieZ Galore",
+                title: "Movies Galore!",
                 name: "Crazpicc's movie gallery",
             })
 })
 
 router.use('/', require('./api/movieRoutes'))
+
+//! 404 ERROR page
+router.use((req, res, next)=> {
+    res.status(404)
+    .render('pages/error404', {
+        title: '404',
+        name: 'THAT PAGE DOES NOT EXIST'
+    })
+})
 
 module.exports = router
